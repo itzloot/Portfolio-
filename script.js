d0da7bbbf1ed234dd6b922e5507e7bb625702db6
itzloot <fahadmalik2712@gmail.com>
2025-12-28 21:13:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 const sections = document.querySelectorAll(".section, .hero");
-
-const observer = new IntersectionObserver(entries => {
-  entries.forEach(e => {
+const observer = new IntersectionObserver(entries=>{
+  entries.forEach(e=>{
     if(e.isIntersecting){
       e.target.style.opacity=1;
       e.target.style.transform="translateY(0)";
     }
   });
 });
-
 sections.forEach(sec=>{
   sec.style.opacity=0;
   sec.style.transform="translateY(30px)";
